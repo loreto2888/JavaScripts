7e60765bced6ef7a055189954c32bfa3e9a77571
Loreto Barrientos Alarcón <50933654+loreto2888@users.noreply.github.com>
2025-07-13 18:33:00 -0400

    Delete Assets directory

--- Assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function pintar(elemento, color = 'green') {
-  elemento.style.backgroundColor = color;
-}
-
-const ele = document.getElementById("ele1");
-ele.addEventListener("click", function(event) {
-  pintar(event.target, 'yellow');
-});
